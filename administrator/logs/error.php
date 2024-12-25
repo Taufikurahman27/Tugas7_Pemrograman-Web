41ee12542e8052a4cce2a41c309d10b304e5b660
Taufikurahman27 <168198355+Taufikurahman27@users.noreply.github.com>
2024-12-25 14:29:52 +0700

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-12-19 02:59:29 UTC
+#Software: Joomla! 5.2.2 Stable [ Uthabiti ] 26-November-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-12-19T02:59:29+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-12-24T06:15:44+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-12-24T06:16:11+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-12-24T06:16:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-12-24T06:18:33+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-12-24T06:19:04+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-12-24 06:15:45 UTC
+#Software: Joomla! 5.2.2 Stable [ Uthabiti ] 26-November-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2024-12-24	06:15:45	INFO	Running task#02 'Session GC'.
+2024-12-24	06:15:45	INFO	Task> SessionGC end
+2024-12-24	06:15:45	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2024-12-24	06:15:48	INFO	Running task#03 'Update Notification'.
+2024-12-24	06:15:49	INFO	Successfully finished task#03 in 1.11 (net 1.11) seconds.
